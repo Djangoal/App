c2919cb1e0c146e4baf46bbaa70069ef68e75113
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-06-11 12:50:36 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from kivy.logger import Logger
+
+
 from views.log_screen import LogScreen
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
@@ -14,6 +17,8 @@ from views.configuration_screen import ConfigurationScreen
 
 class MonApp(App):
     def build(self):
+        Logger.info("MonApp: Un log important")
+        print("Un message dans stdout")
         # Dans ta fonction de création des écrans :
         sm.add_widget(Screen(name='log', content=LogScreen()))
         sm.add_widget(pageprincipalScreen(name="principal"))
