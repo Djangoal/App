418d51bc073f317620c72d6e8d8f78892789e690
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-07-01 08:00:21 +0200

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from kivy.core.window import Window
+Window.softinput_mode = 'below_target'
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from kivy.properties import BooleanProperty
+from views.logs_screen import LogsScreen 
+from views.page_principal_screen import pageprincipalScreen
+from views.revenus_screen import RevenusScreen
+from views.charges_fixes_screen import ChargesFixesScreen
+from views.depenses_screen import DepenseScreen
+from views.configuration_screen import ConfigurationScreen
+import sys
+from logger import logger
+
+def handle_exception(exc_type, exc_value, exc_traceback):
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logger.critical("Exception non gérée", exc_info=(exc_type, exc_value, exc_traceback))
+
+sys.excepthook = handle_exception
+
+
+    
+
+class MonApp(App):
+    show_total_revenus = BooleanProperty(True)
+    show_total_charges = BooleanProperty(True)
+    show_total_depenses = BooleanProperty(True)
+    show_restant_a_payer = BooleanProperty(True)
+
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(pageprincipalScreen(name="principal"))
+        sm.add_widget(ChargesFixesScreen(name="charges_fixe"))
+        sm.add_widget(RevenusScreen(name="revenus"))
+        sm.add_widget(DepenseScreen(name="depense"))
+        sm.add_widget(ConfigurationScreen(name="config"))
+        sm.add_widget(LogsScreen(name="logs"))
+        return sm
+
+if __name__ == "__main__":
+    MonApp().run()
