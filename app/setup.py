4459f18a4b9c6e3e31bc25f7b2882ed3c33948fa
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-08-08 22:21:27 +0200

    Create setup.py

---
+++ app/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='mon_budget_perso',
+    version='0.1',
+    description='Application de gestion de budget personnel',
+    author='Ton Nom',
+    packages=['.'],
+    include_package_data=True,
+)
