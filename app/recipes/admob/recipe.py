ea480176874024830598511f5884b052a6c4f2b0
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-11-14 09:12:29 +0100

    Update recipe.py

--- app/recipes/admob/recipe.py
+++ app/recipes/admob/recipe.py
@@ -1,14 +1,14 @@
 from pythonforandroid.recipe import Recipe
 
-class AdmobRecipe(Recipe):
-    version = '24.3.0'
+class AdMobWrapperRecipe(Recipe):
+    version = '1.0'
     url = None
     depends = ['android']
 
-    # Indique à p4a que ton AAR doit être inclus
-    android_libs = ['play-services-ads-24.3.0.aar']
+    # Indiquer que le Java doit être compilé
+    java_src = ['AdMobBridge.java']
 
     def get_build_dir(self, arch):
         return self.get_recipe_dir()
 
-recipe = AdmobRecipe()
+recipe = AdMobWrapperRecipe()
