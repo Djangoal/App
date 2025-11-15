5705252f5cf440a11531d2f66aa170168a2dcc17
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-11-15 14:40:11 +0100

    Update __init__.py

--- python-for-android/recipes/admob/__init__.py
+++ python-for-android/recipes/admob/__init__.py
@@ -1,20 +1,3 @@
-from pythonforandroid.recipe import Recipe
-import os
+from .recipe import recipe
 
-class AdMobRecipe(Recipe):
-    version = "1.0"
-    url = None  # Pas de téléchargement externe
-    name = "admob"
-    depends = ["python3", "kivy", "android"]
 
-    # Inclure les AAR automatiquement depuis libs/
-    def get_recipe_env(self, arch):
-        env = super().get_recipe_env(arch)
-        return env
-
-    # Optionnel : dossier de build propre
-    def get_build_dir(self, arch):
-        return os.path.join(self.get_build_container_dir(arch), "admob_build")
-
-# ⚠ Obligatoire pour P4A
-recipe = AdMobRecipe()
