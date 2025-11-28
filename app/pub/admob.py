57f29c739357b21ba0fe6a003e961f86bd518054
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-11-28 19:10:34 +0100

    Fix parameter name for new_banner method

--- app/pub/admob.py
+++ app/pub/admob.py
@@ -44,8 +44,8 @@ class AdMobBanner(BoxLayout):
         banner_id = "ca-app-pub-3940256099942544/6300978111"
 
         # Crée une bannière ↓ en bas ↓
-        self.ads.new_banner(banner_id, top=False)
-
+        self.ads.new_banner(banner_id, top_pos=False)
+        
         # Charge la bannière
         self.ads.request_banner()
 
