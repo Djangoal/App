2cb8efa9a4258578c9ee5f0f181942ba438ee7d5
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-11-28 19:34:38 +0100

    Update admob.py

--- app/pub/admob.py
+++ app/pub/admob.py
@@ -44,7 +44,7 @@ class AdMobBanner(BoxLayout):
         banner_id = "ca-app-pub-3940256099942544/6300978111"
 
         # Crée une bannière ↓ en bas ↓
-        self.ads.new_banner(banner_id, top_pos=False)
+        self.ads.new_banner(banner_id, top_pos=True)
         
         # Charge la bannière
         self.ads.request_banner()
