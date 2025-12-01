a34efd0d42cd4045093ac63d27b8b05f36a2b2a1
Djangoal <155944816+Djangoal@users.noreply.github.com>
2025-12-01 16:05:14 +0100

    Update recipe.py

--- app/python-for-android/recipes/google_ads/recipe.py
+++ app/python-for-android/recipes/google_ads/recipe.py
@@ -1,13 +1,12 @@
 from pythonforandroid.recipe import JavaRecipe
 
 class GoogleAdsRecipe(JavaRecipe):
-    name = "google_ads"
     version = "1.0"
+    name = "google_ads"
     src_filename = None
 
     def gradle_dependencies(self):
-        return [
-            "com.google.android.gms:play-services-ads:22.6.0"
-        ]
+        return 
+["com.google.android.gms:play-services-ads:22.6.0"]
 
 recipe = GoogleAdsRecipe()
